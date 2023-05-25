b9310d79ca03e82671e32e7e219c7a3b7ce7ca65
easy <izaaz240@gmail.com>
2023-05-25 19:22:47 +0700

    feat(app): change the functionality to rotate and blur
    rotate the image by 90 degree and then blur it with the degree of 10

--- imageProcessing1/index.js
+++ imageProcessing1/index.js
@@ -13,20 +13,18 @@ app.post("/", (req, res) => {
   res.status(200).json({ message: "success" });
 });
 
-async function process() {
+async function rotateAndBlur() {
   try {
     const result = await sharp("car.jpg")
-      .resize({
-        width: 2000,
-        height: 1000,
-      })
-      .toFile("resized.jpg");
+      .rotate(90, { background: { r: 0, g: 0, b: 0, alpha: 0 } })
+      .blur(10)
+      .toFile("result.jpg");
     console.log(result);
   } catch (e) {
     console.log(`error: ${e}`);
   }
 }
 
-process();
+rotateAndBlur();
 
 app.listen(3000, () => console.log("listen in 3000"));
